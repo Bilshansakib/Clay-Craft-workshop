c383088a925bcfcefe1978aa6a0bf1e9ff701d64
Bilshansakib <burakaghani@gmail.com>
2024-04-26 12:56:16 +0600

    firebase install and setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDh5t3zR11OgwY5_vysAafxXNTEdU-UaM4",
+  authDomain: "clay-craft-workshop.firebaseapp.com",
+  projectId: "clay-craft-workshop",
+  storageBucket: "clay-craft-workshop.appspot.com",
+  messagingSenderId: "711739551991",
+  appId: "1:711739551991:web:85130f8b173e8832e423f2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
